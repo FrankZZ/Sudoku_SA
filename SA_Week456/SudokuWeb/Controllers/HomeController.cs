cc5390d7fb6356cb51ef8c7c2756757ed2e44527
FrankZZ <frank@wamm.es>
2014-06-17 01:12:48 +0200

    Added exceptions Added Back link to Finished/Exception pages

--- SA_Week456/SudokuWeb/Controllers/HomeController.cs
+++ SA_Week456/SudokuWeb/Controllers/HomeController.cs
@@ -12,31 +12,64 @@ namespace SudokuWeb.Controllers
 
 		public ActionResult Index(string btn)
 		{
-			if (btn == "Hint")
-				sb.Hint();
-			else if (Request.QueryString.Count == 3)
+			try 
 			{
-				int x, y, value;
-
-				int.TryParse(Request.QueryString.Get("x"),		out x);
-				int.TryParse(Request.QueryString.Get("y"),		out y);
-				int.TryParse(Request.QueryString.Get("value"),	out value);
-
-				sb.Vakken[x, y].Value = value;
-
-				if (sb.IsFinished())
+				if (Request.QueryString.Count == 1)
 				{
-					sb.Initialize(); // Reset het spel voor de volgende keer
-					Response.RedirectPermanent("/Home/Finished");
+					switch (Request.QueryString.Get("button"))
+					{
+						case "Hint":
+							sb.Hint();
+						break;
+
+						case "Load":
+							sb.Load();
+						break;
+						
+						case "Save":
+							sb.Save();
+						break;
+					}
 				}
+				else if (Request.QueryString.Count == 3)
+				{
+					int x, y, value;
+
+					int.TryParse(Request.QueryString.Get("x"),		out x);
+					int.TryParse(Request.QueryString.Get("y"),		out y);
+					int.TryParse(Request.QueryString.Get("value"),	out value);
+
+					sb.Vakken[x, y].Value = value;
+
+				}
+				
+				if (GameIsFinished())
+				{
+					sb.Initialize();
+
+					return View("Finished");
+				}
+			}
+			catch (Exception ex)
+			{
+				return View("Exception", ex);
 			}
 
 			return View(sb);
 		}
 
-		public ActionResult Finished()
+		private bool GameIsFinished()
 		{
-			return View();
+			for (int x = 0; x < 9; x++)
+			{
+				for (int y = 0; y < 9; y++)
+				{
+					if (sb.GetVak(x, y).Value == 0)
+						return false;
+				}
+			}
+			// Er zijn geen lege plekken meer
+			return sb.IsFinished();
 		}
 
 	}
